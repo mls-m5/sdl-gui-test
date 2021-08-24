339055a3188dd7b9823a019176edc0eb792d1d4c
Mattias Lasersköld <mattias.l.sk@gmail.com>
2021-08-24 14:24:42 +0200

    draw the first whole buttons

--- src/main.cpp
+++ src/main.cpp
@@ -3,6 +3,7 @@
 #include "sdlpp/render.hpp"
 #include "sdlpp/texture.hpp"
 #include "sdlpp/window.hpp"
+#include <array>
 #include <iostream>
 #include <thread>
 
@@ -11,13 +12,79 @@ auto loadTexture(sdl::Renderer &renderer, std::string_view filename)
     return {IMG_LoadTexture(renderer, filename.data())};
 }
 
+void drawButton(sdl::Renderer &renderer,
+                sdl::Texture &texture,
+                int x,
+                int y,
+                int w,
+                int h) {
+    constexpr auto border = 2;
+    const auto halfW = (w - border * 2) / 2; // Will fail sometimes probably
+    const auto halfH = (h - border * 2) / 2; // Will fail sometimes probably
+
+    // Draw all part of the button
+#pragma unloop
+    for (int ny = 0; ny < 4; ++ny) {
+#pragma unloop
+        for (int nx = 0; nx < 4; ++nx) {
+            auto src = sdl::Rect{nx * border, ny * border, border, border};
+            auto dst = sdl::Rect{0, 0, border, border};
+
+            switch (nx) {
+            case 0:
+                break;
+            case 1:
+                dst.x = border;
+                dst.w = halfW;
+                break;
+            case 2:
+                dst.x = halfW + border;
+                dst.w = halfW;
+                break;
+            case 3:
+                dst.x = w - border;
+                break;
+            }
+
+            switch (ny) {
+            case 0:
+                break;
+            case 1:
+                dst.y = border;
+                dst.h = halfH;
+                break;
+            case 2:
+                dst.y = halfH + border;
+                dst.h = halfH;
+                break;
+            case 3:
+                dst.y = h - border;
+                break;
+            }
+
+            dst.x += x;
+            dst.y += y;
+
+            renderer.copy(texture, &src, &dst);
+        }
+    }
+}
+
 int main(int argc, char **argv) {
     using namespace std::chrono_literals;
-    auto window = sdl::Window{"Hello there", 0, 0, 500, 500, SDL_WINDOW_SHOWN};
+    auto window = sdl::Window{"Hello there",
+                              SDL_WINDOWPOS_CENTERED,
+                              SDL_WINDOWPOS_CENTERED,
+                              500,
+                              500,
+                              SDL_WINDOW_SHOWN};
 
     auto renderer = sdl::Renderer{window, -1, SDL_RENDERER_ACCELERATED};
 
-    auto buttonTexture = loadTexture(renderer, "data/button1.png");
+    auto buttonTexture =
+        sdl::Texture{IMG_LoadTexture(renderer, "data/button1.png")};
+    auto buttonTexture2 =
+        sdl::Texture{IMG_LoadTexture(renderer, "data/button2.png")};
 
     if (!buttonTexture) {
         std::cerr << "could not load button1.png\n";
@@ -30,10 +97,11 @@ int main(int argc, char **argv) {
 
     const auto rect = sdl::Rect{0, 0, 8, 8};
 
-    renderer.copy(buttonTexture, nullptr, &rect);
-
     renderer.setDrawColor(100, 100, 100, 200);
     renderer.drawLine(0, 0, 100, 100);
+
+    drawButton(renderer, buttonTexture, 8, 8, 40, 20);
+    drawButton(renderer, buttonTexture2, 8 + 40, 8, 40, 20);
 
     renderer.present();
 
